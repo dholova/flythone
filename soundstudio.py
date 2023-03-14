49d7e374d844371d0035c6df553a49e94354a672
Danya <gds@email.com>
2023-03-14 15:43:53 +0200

    Доповнена функція відправки демо

--- soundstudio.py
+++ soundstudio.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -8,11 +8,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///studio.db'
 db = SQLAlchemy(app)
 
 
-class Signing(db.Model):
+class SendDemo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(50), nullable=False)
-    describe = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(200), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -28,9 +28,22 @@ def studio():
     return render_template('studio.html')
 
 
-@app.route('/send_demo')
+@app.route('/send_demo', methods=['POST', 'GET'])
 def send_demo():
-    return render_template('send_demo.html')
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        email = request.form['email']
+
+        send_demo = SendDemo(name=name, phone=phone, email=email)
+        try:
+            db.session.add(send_demo)
+            db.session.commit()
+            return redirect('/studio')
+        except:
+            return 'При відправці файлу виникла помилка'
+    else:
+        return render_template('send_demo.html')
 
 
 if __name__ == '__main__':
