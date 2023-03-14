9bc54f5c2ab60e3d41ec9f5f04cf53c385262446
Danya <gds@email.com>
2023-03-14 02:20:24 +0200

    Створення папки шаблонів та шаблону для головної сторінки

--- soundstudio.py
+++ soundstudio.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Head page'
-
-
 @app.route('/home')
 def home():
-    return 'Оберіть послугу'
+    return render_template('home.html')
+
+
+@app.route('/studio')
+def studio():
+    return render_template('studio.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
